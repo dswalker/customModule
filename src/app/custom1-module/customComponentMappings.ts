d7867ec69142d6ead90cf3b4716b46d6d0eea950
shoul.manevitz <shoul.manevitz@clarivate.com>
2026-06-07 17:14:21 +0300

    Angular version 20 update

--- src/app/custom1-module/customComponentMappings.ts
+++ src/app/custom1-module/customComponentMappings.ts
@@ -1,12 +1,5 @@
 // Define the map
-import {RecommendationsComponent} from "./recommendations/recommendations.component";
-import {BriefResultComponent} from "./brief-result/brief-result/brief-result.component";
-import {CustomOnlineAvailabilityComponent} from "./custom-online-availability/custom-online-availability.component";
-import {CustomFormControlComponent} from "./customFormControl/custom-form-control/custom-form-control.component";
-
 export const selectorComponentMap = new Map<string, any>([
-  ['nde-search-bar-presenter-after', RecommendationsComponent],
-  ['nde-search-result-item-container-before', BriefResultComponent],
-  ['nde-online-availability-bottom', CustomOnlineAvailabilityComponent],
-  ['nde-base-request-form-after', CustomFormControlComponent]
+
+
 ]);
